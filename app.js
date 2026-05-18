527875db845fb28b23ee1beb90119d9747a25fc8
Buddiga <indira-sai.buddiga@capgemini.com>
2026-05-18 13:45:58 +0530

    updated app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', (req, res) => {
-  res.send('CICD Pipeline Successful')
+  res.send('CICD Pipeline to Automatic Deploymwnt Successful')
 })
  
 app.listen(3000, () => {
